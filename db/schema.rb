48cb20f83239f836036f292d05a5cdf761adeac5
suzukisan22 <getty2017@gmail.com>
2015-07-28 08:47:07 +0000

    add posting microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727084451) do
+ActiveRecord::Schema.define(version: 20150728074503) do
+
+  create_table "add_profile_to_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +33,9 @@ ActiveRecord::Schema.define(version: 20150727084451) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "profile"
+    t.string   "area"
+    t.string   "job"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
